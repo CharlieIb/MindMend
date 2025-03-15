99b02eb649e34277df9ef925e5a99beb1b133f42
Aravind <aravindseeralan57@gmail.com>
2025-03-15 16:11:08 +0000

    Added basic structure of screening tool functionality

---
+++ app/utils/screeningtool.py
@@ -0,0 +1,15 @@
+
+# Sample list of symptoms
+symptom_list = [('1','Symptom 1'),('2','Symptom 2'),('3','Symptom 3'),('4','Symptom 4')]
+
+# Sample questionnaires
+condition1 = [{"qn": "Question1a", "value": 5},{"qn": "Question1b", "value": 3},{"qn": "Question1c", "value": 2}]
+condition2 = [{"qn": "Question2a", "value": 5},{"qn": "Question2b", "value": 3},{"qn": "Question2c", "value": 2}]
+condition3 = [{"qn": "Question3a", "value": 5},{"qn": "Question3b", "value": 3},{"qn": "Question3c", "value": 2}]
+condition4 = [{"qn": "Question4a", "value": 5},{"qn": "Question4b", "value": 3},{"qn": "Question4c", "value": 2}]
+
+questions_database = [{'id':1, 'questions':condition1},
+                      {'id':2, 'questions':condition2},
+                      {'id':3, 'questions':condition3},
+                      {'id':4, 'questions':condition4}
+                      ]
