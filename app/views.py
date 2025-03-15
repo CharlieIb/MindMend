094d0e5ad48162baed130af9272dc63b861f7441
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-03-15 16:03:03 +0000

    Implement heatmap with fake data

--- app/views.py
+++ app/views.py
@@ -204,7 +204,7 @@ def mind_mirror():
         elif form_display.change.data == 'month':
             display = 'year'
     return render_template(
-        'MindMirror.html',
+        'mindmirror.html',
         title='MindMirror',
         curr_day=curr_day,
         curr_month=curr_month,
