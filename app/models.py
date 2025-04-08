381bf53452d26530d900bff95a69812e05a1a8bb
Charlie <charles.ibbett@gmail.com>
2025-04-08 14:34:38 +0100

    SCRUM 48: backref changed to back_populates in models

--- app/models.py
+++ app/models.py
@@ -11,13 +11,20 @@ Base = so.declarative_base()
 
 # User table
 class User(UserMixin, db.Model, Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id: so.Mapped[int] = so.mapped_column(sa.Integer, primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     role: so.Mapped[str] = so.mapped_column(sa.String(24), nullable=False, default='Normal')
+
+    # Relationships:
+    emotion_logs: so.Mapped[list['EmotionLog']] = so.relationship(back_populates="user", cascade="all, delete-orphan")
+    test_result: so.Mapped[list['TestResult']] = so.relationship(back_populates="user", cascade="all, delete-orphan")
+    notifications: so.Mapped[list['Notification']] = so.relationship(back_populates="user", cascade="all, delete-orphan")
+    support_request: so.Mapped['SupportRequest'] = so.relationship(back_populates="user", cascade="all, delete-orphan")
+
 
     def __repr__(self):
         return f"User(id={self.id}, username={self.username}, email={self.email}, role={self.role})"
@@ -42,26 +49,38 @@ class Person(db.Model):
     person_id: so.Mapped[int] = so.mapped_column(sa.Integer, primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(255), unique=True, nullable=False)
 
+    # Relationships:
+    emotion_logs: so.Mapped['EmotionLog'] = so.relationship(back_populates="person")
+
+
 
 # Location Table - could be removed
 class Location(db.Model):
-    __tablename__ = "location"
+    __tablename__ = "locations"
 
     location_id: so.Mapped[int] = so.mapped_column(sa.Integer, primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(255), unique=True, nullable=False)
 
+    # Relationships:
+    emotion_logs: so.Mapped['EmotionLog'] = so.relationship(back_populates="location")
+
+
 
 # Activity Table - could be removed
 class Activity(db.Model):
-    __tablename__ = "activity"
+    __tablename__ = "activities"
 
     activity_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(255), unique=True, nullable=False)
 
+    # Relationships:
+    emotion_logs: so.Mapped['EmotionLog'] = so.relationship(back_populates="activity")
+
+
 
 # EmotionLog Table
 class EmotionLog(db.Model):
-    __tablename__ = "emotion_log"
+    __tablename__ = "emotion_logs"
 
     log_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("user.id"), nullable=False)
@@ -79,37 +98,46 @@ class EmotionLog(db.Model):
     person_id: so.Mapped[int] = so.mapped_column(sa.Integer, sa.ForeignKey("person.person_id"), nullable=True)
 
     # Relationships
-    user = so.relationship("User", backref="emotion_logs")
-    location = so.relationship("Location", backref="emotion_logs")
-    activity = so.relationship("Activity", backref="emotion_logs")
-    people = so.relationship("Person", backref="emotion_logs")
+    user: so.Mapped['User'] = so.relationship(back_populates="emotion_logs")
+    location: so.Mapped['Location'] = so.relationship(back_populates="emotion_logs")
+    activity: so.Mapped['Activity'] = so.relationship(back_populates="emotion_logs")
+    people: so.Mapped['Person'] = so.relationship(back_populates="emotion_logs")
+
+    # user = so.relationship("User", backref="emotion_logs")
+    # location = so.relationship("Location", backref="emotion_logs")
+    # activity = so.relationship("Activity", backref="emotion_logs")
+    # people = so.relationship("Person", backref="emotion_logs")
 
 
 ##Screening Tool tables
 # Condition Table
 class Condition(db.Model):
-    __tablename__ = "condition"
+    __tablename__ = "conditions"
 
     cond_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=False, unique=True)
     threshold: so.Mapped[int] = so.mapped_column(sa.Integer, nullable=False)
 
+
+    # Relationships:
+    questions: so.Mapped[list['ConditionQuestion']] = so.relationship(back_populates="condition", cascade="all, delete-orphan")
+    test_result: so.Mapped[list['TestResult']] = so.relationship(back_populates="condition", cascade="all, delete-orphan")
+
+
     # Relationships through secondary tables
-    therapeutic_recs = so.relationship(
-        "TherapeuticRec",
+    therapeutic_recs: so.Mapped[list['TherapeuticRec']] = so.relationship(
         secondary="therapeutic_rec_condition",
-        backref="conditions"
+        back_populates="conditions"
     )
-    resources = so.relationship(
-        "Resource",
+    resources: so.Mapped[list['Resource']] = so.relationship(
         secondary="resource_condition",
-        backref="conditions"
+        back_populates="conditions"
     )
 
 
 # Condition Questionnaire Questions
 class ConditionQuestion(db.Model):
-    __tablename__ = "condition_question"
+    __tablename__ = "condition_questions"
 
     cond_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("condition.cond_id"), primary_key=True)
     q_number: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -117,12 +145,14 @@ class ConditionQuestion(db.Model):
     value: so.Mapped[int] = so.mapped_column(sa.Integer, nullable=False)
 
     # Relationships
-    condition = so.relationship("Condition", backref="questions")
+    condition: so.Mapped['Condition'] = so.relationship(back_populates="questions")
+
+    # condition = so.relationship("Condition", backref="questions")
 
 
 # Test Result Table
 class TestResult(db.Model):
-    __tablename__ = "test_result"
+    __tablename__ = "test_results"
 
     test_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("user.id"), nullable=False)
@@ -131,13 +161,17 @@ class TestResult(db.Model):
     timedate: so.Mapped[sa.DateTime] = so.mapped_column(sa.DateTime, default=datetime.utcnow)
 
     # Relationships
-    user = so.relationship("User", backref="test_results")
-    condition = so.relationship("Condition", backref="test_results")
+    user: so.Mapped['User'] = so.relationship(back_populates="test_result")
+    condition: so.Mapped['Condition'] = so.relationship(back_populates="test_result")
+
+
+    #user = so.relationship("User", backref="test_results")
+    #condition = so.relationship("Condition", backref="test_results")
 
 
 # Therapeutic Recommendations Table
 class TherapeuticRec(db.Model):
-    __tablename__ = "therapeutic_rec"
+    __tablename__ = "therapeutic_recs"
 
     rec_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     description: so.Mapped[str] = so.mapped_column(sa.Text, nullable=False, unique=True)
@@ -145,19 +179,31 @@ class TherapeuticRec(db.Model):
     source: so.Mapped[str] = so.mapped_column(sa.String(256))
     treatments: so.Mapped[str] = so.mapped_column(sa.Text)
 
+    # Relationship:
+    conditions: so.Mapped[list['Condition']] = so.relationship(
+        secondary="therapeutic_rec_condition",
+        back_populates="conditions"
+    )
+
 
 # Resource Table
 class Resource(db.Model):
-    __tablename__ = "resource"
+    __tablename__ = "resources"
 
     resource_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     label: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=False, unique=True)
     link: so.Mapped[str] = so.mapped_column(sa.Text, nullable=False)
 
+    # Relationship:
+    conditions: so.Mapped[list['Condition']] = so.relationship(
+        secondary="resource_condition",
+        back_populates="conditions"
+    )
+
 
 # Associate table: Therapeutic Rec and Condition
 therapeutic_rec_condition = sa.Table(
-    "therapeutic_rec_condition",
+    "therapeutic_rec_conditions",
     db.metadata,
     sa.Column("therapeutic_id", sa.ForeignKey("therapeutic_rec.rec_id"), primary_key=True),
     sa.Column("condition_id", sa.ForeignKey("condition.cond_id"), primary_key=True)
@@ -165,7 +211,7 @@ therapeutic_rec_condition = sa.Table(
 
 # Association table: Resource and Condition
 resource_condition = sa.Table(
-    "resource_condition",
+    "resource_conditions",
     db.metadata,
     sa.Column("resource_id", sa.ForeignKey("resource.resource_id"), primary_key=True),
     sa.Column("condition_id", sa.ForeignKey("condition.cond_id"), primary_key=True)
@@ -175,7 +221,7 @@ resource_condition = sa.Table(
 ## Mind Mirror
 # Notifications
 class Notification(db.Model):
-    __tablename__ = "notification"
+    __tablename__ = "notifications"
 
     notification_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("user.id"), nullable=False)
@@ -184,13 +230,13 @@ class Notification(db.Model):
     is_read: so.Mapped[bool] = so.mapped_column(sa.Boolean, default=False)
 
     # Relationships
-    user = so.relationship("User", backref="notifications")
+    user: so.Mapped['User'] = so.relationship(back_populates="notifications")
 
 
 ## ReachOut
 # SupportRequest
 class SupportRequest(db.Model):
-    __tablename__ = "support_request"
+    __tablename__ = "support_requests"
 
     request_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("user.id"), nullable=False)
@@ -198,4 +244,4 @@ class SupportRequest(db.Model):
     description: so.Mapped[str] = so.mapped_column(sa.Text, nullable=False)
 
     # Relationships
-    user = so.relationship("User", backref="support_requests")
+    user: so.Mapped['User'] = so.relationship(back_populates="support_request")
