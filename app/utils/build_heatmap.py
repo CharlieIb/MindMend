de579a208f73bedbf330c22bd5f54037e1713993
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-04-12 21:19:59 +0100

    Link and manage db data to show and output desired result

--- app/utils/build_heatmap.py
+++ app/utils/build_heatmap.py
@@ -12,7 +12,7 @@ class HeatMap:
 
     def month_display(self):
         month_info = [[self.mm, self.yy], calendar.monthcalendar(self.yy, self.mm)]
-        logs_mapping = self._create_logs_mapping(self.data_log)
+        logs_mapping = self._create_logs_mapping(self.group_emotions() or self.data_log)
         for week in month_info[1]:
             for i, day in enumerate(week):
                 if day != 0 and (day, self.mm, self.yy) in logs_mapping:
@@ -20,7 +20,7 @@ class HeatMap:
         return month_info
 
     def year_display(self):
-        logs_mapping = self._create_logs_mapping(self.data_log)
+        logs_mapping = self._create_logs_mapping(self.group_emotions() or self.data_log)
         days_in_year = []
         for m in range(1, 13):
             month_cal = calendar.monthcalendar(self.yy, m)
@@ -30,6 +30,30 @@ class HeatMap:
                         week[i] = [week[i], logs_mapping[(day, m, self.yy)]]
             days_in_year.append([[m, self.yy], month_cal])
         return days_in_year
+
+    def group_emotions(self):
+        emotions_table = [
+            {'emotion': 'Happy', 'colour': 'bright_yellow'},
+            {'emotion': 'Calm', 'colour': 'light_blue'},
+            {'emotion': 'Stressed', 'colour': 'grey'},
+            {'emotion': 'Energetic', 'colour': 'bright_orange'},
+            {'emotion': 'Anxious', 'colour': 'red'},
+            {'emotion': 'Sad', 'colour': 'dark_blue'},
+            {'emotion': 'Excited', 'colour': 'hot_pink'},
+            {'emotion': 'Angry', 'colour': 'dark_red'}
+        ]
+
+        group_data = []
+        for log in self.data_log:
+            data = {'date': log['date'], 'emotion': log['emotion']}
+            for info in emotions_table:
+                if log['emotion'] == info['emotion']:
+                    data['colour'] = info['colour']
+                    break
+            if len(data) == 3:
+                group_data.append(data)
+
+        return group_data
 
     @staticmethod
     def fake_data_log_raw():
