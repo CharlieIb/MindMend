08d1959f38393c64845ef8eb522e6d6f976e405d
Romas <romasalmalhi@gmail.com>
2025-04-15 19:06:26 +0100

    fix(symptoms): update presenting complaints to accurate values
    - Replaced previous arbitrary symptom values with corrected, clinically relevant ones

--- app/utils/screeningtool.py
+++ app/utils/screeningtool.py
@@ -1,6 +1,10 @@
 
 # Sample list of symptoms
-symptom_list = [('1','Symptom 1'),('2','Symptom 2'),('3','Symptom 3'),('4','Symptom 4'),('5','Symptom 5'),('6','Symptom 6'),('7','Symptom 7'),('8','Symptom 8'),('9','Symptom 9'),('10','Symptom 10')]
+symptom_list = [('1','Excessive Worry/Anxiety'),('2','Panic Attack/Intense Fear'),('3','Fear/Intense Discomfort in Social Settings'),
+                ('4','Avoidance of Social Situations'),('5','Low Mood'),('6','No Enjoyment in Anything'),('7','Low Energy/Fatigue'),
+                ('8','Poor Concentration'),('9','Fluctuating Mood'),('10','Incredibly (unusually) Energetic'),
+                ('11','Intentional Weight Loss (Large Amount)'), ('12','Intense Fear of Weight Gain'), ('13','Very Negative Body Image'),
+                ('14','Trouble Quitting a Substance'), ('15','Physical Self Harm to Oneself')]
 
 # Sample questionnaires
 condition1 = [{"qn": "Question1a", "value": 5},{"qn": "Question1b", "value": 3},{"qn": "Question1c", "value": 2}]
